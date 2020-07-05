3492f0fae961605e6d41cc9dc44262d472f3f684
kruserr <46799551+kruserr@users.noreply.github.com>
2020-07-05 22:44:00 +0000

    added more tests

--- tests/unit/cli/test_cli.py
+++ tests/unit/cli/test_cli.py
@@ -1,8 +1,21 @@
 import i6
 
-import pytest
 import os
+import pytest
+import subprocess
 
 
 def test_cli():
     i6.cli(menu=False).run()
+
+def test_cli_subprocess_direct():
+    subprocess.run(['python', 'src/i6/cli/main.py'], timeout=1)
+
+def test_cli_subprocess_path():
+    subprocess.run(['i6'], timeout=1)
+
+def test_cli_subprocess_python_module():
+    subprocess.run(['python', '-m', 'i6'], timeout=1)
+
+def test_cli_args():
+    i6.cli('aiocheck', menu=False).run()
